37211c81e87ebeadd0507376c90a2828637067f8
mouwrice <mauricevanwassenhove@gmail.com>
2023-12-05 12:48:13 +0100

    feat: plotting

--- src/csv_objects/csv_comparison.py
+++ src/csv_objects/csv_comparison.py
@@ -1,5 +1,16 @@
 import csv
-from dataclasses import dataclass
+
+from bokeh.io import show, output_file
+from bokeh.plotting import figure
+
+qtm_to_mediapipe = {
+    0: 19,  # left index
+    1: 15,  # left wrist
+    2: 20,  # right index
+    3: 16,  # right wrist
+    4: 31,  # left foot index
+    5: 32,  # right foot index
+}
 
 
 class CSVRow:
@@ -67,9 +78,66 @@ def into_frames(csv_file: str) -> list[Frame]:
     return frames
 
 
-def compare_and_plot_csv_files(csv1: str, csv2: str, frame_offset1: int = 0, frame_offset2: int = 0):
+def plot_positions(positions: list[list[CSVRow]], markers: list[int] = None):
+    """
+    Plot the positions of the markers over time
+    :param markers: Filter for markers, if None all markers are plotted, else only the markers in the list are plotted
+    :param positions:
+    :return:
+    """
+
+    if markers is None:
+        markers = range(len(positions[0]))
+    for i in markers:
+        plot = figure(title=f"Position of marker {i} over time", x_axis_label='Frame', y_axis_label='Position',
+                      sizing_mode="stretch_both")
+        plot.line(list(range(len(positions))), [position[i].x for position in positions], legend_label="x",
+                  line_color="red")
+        plot.line(list(range(len(positions))), [position[i].y for position in positions], legend_label="y",
+                  line_color="green")
+        plot.line(list(range(len(positions))), [position[i].z for position in positions], legend_label="z",
+                  line_color="blue")
+        output_file(f"marker_{i}.html")
+        show(plot)
+
+
+def plot_distances(distances: list[list[float]], mapping: list[int] = None):
+    """
+    Plot the distances between markers over time
+    :param distances:
+    :return:
+    """
+    plot = figure(title="Distance between markers over time", x_axis_label='Frame', y_axis_label='Distance',
+                  sizing_mode="stretch_both")
+    colors = ["red", "blue", "green", "orange", "purple", "brown", "pink", "gray", "black"]
+    for i in range(len(distances[0])):
+        plot.line(list(range(len(distances))), [distance[i] for distance in distances],
+                  legend_label=f"Marker {i}-{mapping[i]}",
+                  line_color=colors[i % len(colors)])
+    output_file("distances.html")
+    show(plot)
+
+
+def row_distance(row1: CSVRow, row2: CSVRow) -> float:
+    """
+    Calculate the distance between two rows
+    :param row1:
+    :param row2:
+    :return:
+    """
+    return ((row1.x - row2.x) ** 2 + (row1.y - row2.y) ** 2 + (row1.z - row2.z) ** 2) ** 0.5
+
+
+def compare_and_plot_csv_files(csv1: str, csv2: str, frame_offset1: int = 0, frame_offset2: int = 0,
+                               mapping: dict = None):
     """
     Compare two CSV files and plot the differences
+    :param csv1: path to the first CSV file
+    :param csv2: path to the second CSV file
+    :param frame_offset1: offset for the first CSV file
+    :param frame_offset2: offset for the second CSV file
+    :param mapping: mapping from trackers of the first CSV file to the second CSV file
+    if None, all trackers are compared based on their index
     """
     frames1 = into_frames(csv1)
     frames2 = into_frames(csv2)
@@ -78,15 +146,20 @@ def compare_and_plot_csv_files(csv1: str, csv2: str, frame_offset1: int = 0, fra
     frames2_index = frame_offset2
     time_offset = frames1[frames1_index].time_ms - frames2[frames2_index].time_ms
 
+    distances = []
+
     frame1 = frames1[frames1_index]
     frame2 = frames2[frames2_index]
+    positions1 = []
+    positions2 = []
     while frames1_index < len(frames1) - 1 and frames2_index < len(frames2) - 1:
         frame1_next = frames1[frames1_index + 1]
         frame2_next = frames2[frames2_index + 1]
-        if frame1.time_ms < frame2.time_ms + time_offset:
+        frame2_offset_time = frame2.time_ms + time_offset
+        if frame1.time_ms < frame2_offset_time:
             frames1_index += 1
             frame1 = frame1_next
-        elif frame1.time_ms > frame2.time_ms + time_offset:
+        elif frame1.time_ms > frame2_offset_time:
             frames2_index += 1
             frame2 = frame2_next
         else:
@@ -95,14 +168,37 @@ def compare_and_plot_csv_files(csv1: str, csv2: str, frame_offset1: int = 0, fra
             frame1 = frame1_next
             frame2 = frame2_next
 
-        for row1 in frame1.rows:
-            for row2 in frame2.rows:
-                if row1.index == row2.index:
-                    print(f"Frame: {frame1.frame}, Marker: {row1.index}, "
-                          f"Distance: {((row1.x - row2.x) ** 2 + (row1.y - row2.y) ** 2 + (row1.z - row2.z) ** 2) ** 0.5}")
-        print(f"Frame: {frame1.frame}, Time: {frame1.time_ms}, Time: {frame2.time_ms + time_offset}")
-        print()
+        positions1.append(frame1.rows)
+        positions2.append(frame2.rows)
+
+        if mapping is None:
+            frame_distances = []
+            for i in range(min(len(frame1.rows), len(frame2.rows))):
+                row1 = frame1.rows[i]
+                row2 = frame2.rows[i]
+                distance = row_distance(row1, row2)
+                frame_distances.append(distance)
+                # print(f"Frame: {frame1.frame}, Marker: {row1.index}, "
+                #       f"Distance: {distance}")
+            distances.append(frame_distances)
+        else:
+            frame_distances = []
+            for key, value in mapping.items():
+                row1 = frame1.rows[key]
+                row2 = frame2.rows[value]
+                distance = row_distance(row1, row2)
+                frame_distances.append(distance)
+                # print(f"Frame: {frame1.frame}, Marker: {row1.index} - {row2.index}, "
+                #       f"Distance: {distance}")
+            distances.append(frame_distances)
+
+        # print(f"Frame: {frame1.frame}, Time: {frame1.time_ms}, Time: {frame2.time_ms + time_offset}")
+        # print()
+    plot_positions(positions1, markers=[0, 1, 2, 3, 4, 5])
+    plot_positions(positions2, markers=[19])
+    plot_distances(distances, mapping=list(qtm_to_mediapipe.values()))
 
 
 if __name__ == '__main__':
-    compare_and_plot_csv_files("qtm_multicam_1.csv", "mediapipe_LITE_multicam_1_left.csv", frame_offset1=5)
+    compare_and_plot_csv_files("qtm_multicam_1.csv", "mediapipe_LITE_multicam_1_left.csv", frame_offset1=5,
+                               mapping=qtm_to_mediapipe)
