77361f6cd3afecaa1fcd0c61669747209382388d
Mouwrice <mauricevanwassenhove@fastmail.com>
2024-05-01 15:52:21 +0200

    fix: .task files path

--- drumpy/mediapipe_pose/landmarker_model.py
+++ drumpy/mediapipe_pose/landmarker_model.py
@@ -3,5 +3,5 @@ from enum import Enum
 
 class LandmarkerModel(Enum):
     LITE = "./resources/pose_landmarker_models/pose_landmarker_lite.task"
-    FULL = "./pose_landmarker_models/pose_landmarker_full.task"
-    HEAVY = "./pose_landmarker_models/pose_landmarker_heavy.task"
+    FULL = "./resources/pose_landmarker_models/pose_landmarker_full.task"
+    HEAVY = "./resources/pose_landmarker_models/pose_landmarker_heavy.task"
