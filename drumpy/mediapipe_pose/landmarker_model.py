adbbdc784e09194b827c800a570faed4b5a6fffc
Maurice Van Wassenhove <mauricevanwassenhove@fastmail.com>
2024-05-28 15:41:27 +0200

    fix: rename mediapipe_pose module to pose

--- drumpy/mediapipe_pose/landmarker_model.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class LandmarkerModel(Enum):
-    LITE = "./resources/pose_landmarker_models/pose_landmarker_lite.task"
-    FULL = "./resources/pose_landmarker_models/pose_landmarker_full.task"
-    HEAVY = "./resources/pose_landmarker_models/pose_landmarker_heavy.task"
